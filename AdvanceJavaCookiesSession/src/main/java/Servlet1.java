a7ff5eaaf1e8011f28ea54e225c5a63feead47a5
azartechnet <azartechnet@gmail.com>
2025-06-30 13:13:34 +0530

    updated

---
+++ AdvanceJavaCookiesSession/src/main/java/Servlet1.java
@@ -0,0 +1,39 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet1
+ */
+@WebServlet("/Servlet1")
+public class Servlet1 extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String u=req.getParameter("uname");
+		
+		PrintWriter out=resp.getWriter();
+		
+		if(u.equals("admin"))
+		{
+			Cookie c=new Cookie("u1", u);
+			resp.addCookie(c);
+			RequestDispatcher f1=req.getRequestDispatcher("Servlet2");
+			f1.forward(req, resp);
+		}
+		else
+		{
+			out.println("Login Fail!!!");
+		}
+	}
+}
---
+++ AdvanceJavaHiddenForm/src/main/java/SecondServlet.java
@@ -0,0 +1,27 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SecondServlet
+ */
+@WebServlet("/SecondServlet")
+public class SecondServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		PrintWriter out=resp.getWriter();
+		
+		String j=req.getParameter("ename");
+		
+		out.println("Your data is::"+j);
+	}
+}
