5b5a566234e019db644ba121de12e5ab0d3d242c
azartechnet <azartechnet@gmail.com>
2025-09-25 13:06:08 +0530

    updated

---
+++ SpringBootWebP1/src/main/java/com/example/SpringBootWebP1/SpringBootWebP1Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootWebP1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebP1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebP1Application.class, args);
+	}
+
+}
