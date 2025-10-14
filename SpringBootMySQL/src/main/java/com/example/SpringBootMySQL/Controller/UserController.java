97e5bf08fdfed0c192ba2e15e4900c34781d0e5b
azartechnet <azartechnet@gmail.com>
2025-10-14 12:43:13 +0530

    updated

--- SpringBootMySQL/src/main/java/com/example/SpringBootMySQL/Controller/UserController.java
+++ SpringBootMySQL/src/main/java/com/example/SpringBootMySQL/Controller/UserController.java
@@ -3,8 +3,12 @@ package com.example.SpringBootMySQL.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +17,7 @@ import com.example.SpringBootMySQL.model.User;
 import com.example.SpringBootMySQL.service.UserService;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
 public class UserController {
 
 	
@@ -29,4 +33,36 @@ public class UserController {
     public User createUser(@RequestBody User user) {
 	     return userService.createUser(user);
 	    }
+	 
+	 @GetMapping("/{id}")
+	 public ResponseEntity<User>getUserById(@PathVariable Long id)
+	 {
+		 User user=userService.getUserById(id);
+		 if(user!=null)
+		 {
+			 return ResponseEntity.ok(user);
+		 }
+		 return ResponseEntity.notFound().build();
+	 }
+	 
+	 
+	 @PutMapping("/{id}")
+	 public ResponseEntity<User>updateUser(@PathVariable Long id,@RequestBody User userDetails)
+	 {
+		 User updatedUser=userService.updateUser(id,userDetails);
+		 if(updatedUser!=null)
+		 {
+			 return ResponseEntity.ok(updatedUser);
+		 }
+		 return ResponseEntity.notFound().build();
+	 }
+	 
+	 @DeleteMapping("/{id}")
+	 public ResponseEntity<Void>deleteUser(@PathVariable Long id)
+	 {
+		 userService.deleteUser(id);
+		 return ResponseEntity.noContent().build();
+	 }
+	
+	 
 }
--- SpringBootMySQL/src/main/java/com/example/SpringBootMySQL/service/UserService.java
+++ SpringBootMySQL/src/main/java/com/example/SpringBootMySQL/service/UserService.java
@@ -26,9 +26,32 @@ public class UserService implements UserRepository
     public User createUser(User user) {
         return userRepository.save(user);
     }
-
     
+    public User getUserById(Long id)
+    {
+    	return userRepository.findById(id).orElse(null);
+    }
 
+    public User updateUser(Long id,User userDetails)
+    {
+    	User user=userRepository.findById(id).orElse(null);
+    	if(user!=null)
+    	{
+    		user.setName(userDetails.getName());
+    		user.setEmail(userDetails.getEmail());
+    		return userRepository.save(user);
+    	}
+    	return null;
+    }
+    
+    
+    public void deleteUser(Long id)
+    {
+    	userRepository.deleteById(id);
+    }
+    
+    
+    
 	@Override
 	public <S extends User> S save(S entity) {
 		// TODO Auto-generated method stub
@@ -101,6 +124,7 @@ public class UserService implements UserRepository
 		
 	}
 
-	
+    
+
     
 }
